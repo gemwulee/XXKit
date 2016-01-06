de1b6a87a966ae2b0c2a806091c7fa1ba9f887e9
tomxiang <tomxiang@tomxiangdeiMac.local>
2016-01-06 11:14:34 +0800

    全部文件提交

---
+++ XXBase/XXBase/XXBase.h
@@ -0,0 +1,11 @@
+//
+//  XXBase.h
+//  XXBase
+//
+//  Created by tomxiang on 16/1/6.
+//  Copyright © 2016年 tomxiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "kindextension/NSStringEx.h"
