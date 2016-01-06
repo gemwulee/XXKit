6e3d4f3136aaf0254c712f6c3d7eb29adbc65992
tomxiang <tomxiang@tomxiangdeimac.local>
2016-01-06 11:02:48 +0800

    Initial Commit

---
+++ XXKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXKit
+//
+//  Created by tomxiang on 16/1/6.
+//  Copyright © 2016年 tomxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
